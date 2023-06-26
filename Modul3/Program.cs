b26267919ec224e90a5c71d575977c5680e245d1
Repnikov.A <repnikov.a@florcom.ru>
2023-06-26 16:21:39 +0300

    Самостоятельный ввод имени с клавиатуры

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -4,8 +4,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        double result = 10 % 3;
-        Console.WriteLine(result);
+        Console.Write ("What is your name?: ");
+        var name = Console.ReadLine ();
+        Console.Write("What is your age?: ");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+        Console.Write("What is your favorite day of week? ");
+
+        var day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day is {0}", day);
         Console.ReadKey();
     }
 }
