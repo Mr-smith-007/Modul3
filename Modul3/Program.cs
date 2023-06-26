3ef65d919cfd092ec27ace98b9b651e66f0e45c3
Repnikov.A <repnikov.a@florcom.ru>
2023-06-26 10:01:04 +0300

    Создать перечисление дней недели

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -5,15 +5,16 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
-
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
+        enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday    
+    }
 
     }
 }
