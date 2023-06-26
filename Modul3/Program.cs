fcf9604cf30b1b4164dc917549e87a91605ea9c2
Repnikov.A <repnikov.a@florcom.ru>
2023-06-26 15:26:37 +0300

    Задание 3.6.7 и 3.6.8

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -4,12 +4,8 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-
-        string name = "Jane";
-        byte age = 27;
-        string favcolor = "black";
-        Console.WriteLine("{0} \n  {1} \n {2}", name, age, favcolor);
-
+        double result = 10 % 3;
+        Console.WriteLine(result);
         Console.ReadKey();
     }
 }
