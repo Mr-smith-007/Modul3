218e7a9193808bd35a2fc322db6cc7cab7fdcd14
Repnikov.A <repnikov.a@florcom.ru>
2023-06-26 15:34:23 +0300

    Revert "Задание 3.6.7"
    This reverts commit bfa7530f416c42437adb0248ae7aba0d2632794f.

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -4,8 +4,12 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        double result = 10 % 3;
-        Console.WriteLine(result);
+
+        string name = "Jane";
+        byte age = 27;
+        string favcolor = "black";
+        Console.WriteLine("{0} \n  {1} \n {2}", name, age, favcolor);
+
         Console.ReadKey();
     }
 }
