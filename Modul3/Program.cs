4534466ff9152f16bdc163c33bd0622099f04503
Repnikov.A <repnikov.a@florcom.ru>
2023-06-26 16:31:45 +0300

    Итоговое 3.7

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -4,16 +4,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.Write ("What is your name?: ");
+        Console.Write ("Enter your name: ");
         var name = Console.ReadLine ();
-        Console.Write("What is your age?: ");
+        Console.Write("Enter your age: ");
         var age = checked((byte)int.Parse(Console.ReadLine()));
         Console.WriteLine("Your name is {0} and age is {1} ", name, age);
 
-        Console.Write("What is your favorite day of week? ");
+        Console.Write("Enter your birthdate: ");
 
-        var day = (DayOfWeek)int.Parse(Console.ReadLine());
-        Console.WriteLine("Your favorite day is {0}", day);
+        var birthdate = Console.ReadLine();
+        Console.WriteLine($"Your birthdate is {birthdate}");
         Console.ReadKey();
     }
 }
